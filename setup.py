5aac6b300426acff276b8367610a45861962fb6d
iliya <iliya@pop-os.localdomain>
2021-10-25 18:46:29 +0330

    add installing requirements

--- setup.py
+++ setup.py
@@ -1,23 +1,26 @@
 import setuptools
 
-with open("README.md", "r") as file:
+with open("README.md") as file:
     long_description = file.read()
 
+with open("requirements.txt") as file:
+    requirements = file.read().splitlines()
 
 setuptools.setup(
-     name='remove-apt-repository',
-     version='1.0',
-     scripts=['remove-apt-repository'] ,
-     author="Iliya Rezaee",
-     author_email="iliyarezaee2017@gmail.com",
-     description="a smal tool for remove apt repositories",
-     long_description=long_description,
-     long_description_content_type="text/markdown",
-     url="https://github.com/IliyaRezaee/remove-apt-repository",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "Operating System :: POSIX :: Linux",
-         "Environment :: Console",
-     ],
- )
+    name='remove-apt-repository',
+    version='1.1',
+    scripts=['remove-apt-repository'] ,
+    author="Iliya Rezaee",
+    author_email="iliyarezaee2017@gmail.com",
+    description="a smal tool for remove apt repositories",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/IliyaRezaee/remove-apt-repository",
+    packages=setuptools.find_packages(),
+    install_requires = requirements,
+    classifiers=[
+     "Programming Language :: Python :: 3",
+     "Operating System :: POSIX :: Linux",
+     "Environment :: Console",
+    ]
+)
